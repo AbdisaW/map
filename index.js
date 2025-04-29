050e8caf880620a2b9e19b59f07da995324123e8
Abdi <abdisawake5@gmail.com>
2025-04-29 09:13:45 +0300

    Exe 1 in the factory and conustructor

--- index.js
+++ index.js
@@ -1,11 +1,73 @@
-function Person(name, age){
-  this.name =name;
-  this.age =age;
-  this.greet=function() {
-   console.log(`Hi, I'm ${this.name} and ${this.age} years`);   
-  }
+// function createBook(title,author,pages){
+//   return{
+//     title,
+//     author,
+//     pages,
+//     describe(){
+//       console.log(`The book ${this.title} by ${this.author} has ${pages} pages.`)
+//     }
+//   }
+// }
+
+// const book = createBook('Harry', 'J.K. Rowling', 500);
+// book.describe();
+
+// function createCounter(count){
+//   return{
+   
+//     increment(){
+//       count++;
+
+//     },
+//     decrement(){
+//       count--;
+//     },
+//     getCount(){
+//       return count;
+//     }
+
+//   }
+
+// }
+
+// const counter = createCounter(0);
+// counter.increment();
+// counter.increment();
+// console.log(counter.getCount());
+// counter.decrement();
+// console.log(counter.getCount());
+
+
+// function Student(name, grade){
+  
+//     this.name = name;
+//     this.grade =grade;
+//     this.study =function(){
+//       console.log(`Student ${this.name} is studying for grade ${this.grade}`)
+//     }
+// }
+
+// const student = new Student('John', 10);
+
+// student.study();
+
+
+// function Circle(radius){
+//  this.radius =radius;
+//  this.area =function(){
+//    console.log(Math.PI *radius*radius) 
+//  }
+// }
+// const c =new Circle(5)
+// c.area();
+
+function Circle(radius) {
+  this.radius = radius;
 }
 
-const person1 = new Person('Abdi', 25)
+Circle.prototype.area = function() {
+  console.log(Math.PI * this.radius * this.radius);
+};
 
-person1.greet();
+const c1 = new Circle(5);
+c1.area();
