d9ee7f483c67ae012c211eca4e8a045cc56d537f
Abdi <abdisawake5@gmail.com>
2025-04-29 09:55:36 +0300

    conustructor function and factory function

--- index.js
+++ index.js
@@ -1,73 +1,26 @@
-// function createBook(title,author,pages){
-//   return{
-//     title,
-//     author,
-//     pages,
-//     describe(){
-//       console.log(`The book ${this.title} by ${this.author} has ${pages} pages.`)
-//     }
-//   }
-// }
 
-// const book = createBook('Harry', 'J.K. Rowling', 500);
-// book.describe();
+// Factory function
 
-// function createCounter(count){
-//   return{
-   
-//     increment(){
-//       count++;
+function createAddress(street, city, ZipCode){
+  return{
+    street,
+    city,
+    ZipCode
+  }
 
-//     },
-//     decrement(){
-//       count--;
-//     },
-//     getCount(){
-//       return count;
-//     }
-
-//   }
-
-// }
-
-// const counter = createCounter(0);
-// counter.increment();
-// counter.increment();
-// console.log(counter.getCount());
-// counter.decrement();
-// console.log(counter.getCount());
+}
+const address = createAddress('a', 'b', 'c');
+console.log(address);
 
 
-// function Student(name, grade){
-  
-//     this.name = name;
-//     this.grade =grade;
-//     this.study =function(){
-//       console.log(`Student ${this.name} is studying for grade ${this.grade}`)
-//     }
-// }
 
-// const student = new Student('John', 10);
-
-// student.study();
-
-
-// function Circle(radius){
-//  this.radius =radius;
-//  this.area =function(){
-//    console.log(Math.PI *radius*radius) 
-//  }
-// }
-// const c =new Circle(5)
-// c.area();
-
-function Circle(radius) {
-  this.radius = radius;
+// Counstroctor function
+function Address(street, city, ZipCode){
+  this.street=street;
+  this.city = city;
+  this.ZipCode=ZipCode
 }
 
-Circle.prototype.area = function() {
-  console.log(Math.PI * this.radius * this.radius);
-};
+const address1 = new Address('a', 'b', 'c')
 
-const c1 = new Circle(5);
-c1.area();
+console.log(address1);
