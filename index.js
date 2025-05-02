a1b5e0551cf6ea11706a29841cb2b3debc7f8d1d
Abdi <abdisawake5@gmail.com>
2025-05-02 21:24:19 +0300

    check all are postive

--- index.js
+++ index.js
@@ -1,16 +1,9 @@
 
-const courses =[
-    {id:1, name: "Node.js" },
-    {id:2, name: "javaScript" }
-]
-
-courses.sort(function(a, b){
-    const nameA = a.name.toLocaleLowerCase();
-    const nameB = b.name.toLocaleLowerCase();
-    if(nameA < nameB ) return -1;
-    if(nameA > nameB) return 1;
-    return 0;
+const numbers=[1, 2, -1, 3, 4];
+// every
+// some
+const allPostive= numbers.every(function(value){
+    return value >= 0;
 })
 
-
-console.log(courses);
+console.log(allPostive)
