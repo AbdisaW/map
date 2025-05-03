ce7a4a9c56d2e7b815df31f6a527a1efe88c4b7b
Abdi <abdisawake5@gmail.com>
2025-05-03 06:50:33 +0300

    filter the array

--- index.js
+++ index.js
@@ -1,9 +1,7 @@
 
 const numbers=[1, 2, -1, 3, 4];
-// every
-// some
-const allPostive= numbers.every(function(value){
-    return value >= 0;
-})
 
-console.log(allPostive)
+const filtered =numbers.filter(function(value){
+    return value > 2
+})
+console.log(filtered)
