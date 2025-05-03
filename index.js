808aefbbeead5777810cb0c8c95ff05aa0f2305a
Abdi <abdisawake5@gmail.com>
2025-05-03 07:27:04 +0300

    maps of array

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 
 const numbers=[1, 2, -1, 3, 4];
 
-const filtered =numbers.filter(function(value){
-    return value > 2
-})
-console.log(filtered)
+const items =numbers
+    .filter(n => n >= 0)
+    .map(n => ({ a: n}));
+
+
+console.log(items)
